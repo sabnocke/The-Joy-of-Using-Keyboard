9996a0642da1e5954e522ee65f002f77717617e7
sabnocke <hloch.michal@gmail.com>
2023-01-06 18:02:04 +0100

    update

---
+++ C# more like E/stack/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack(1);
+            stack.Push(9);
+            stack.Push(10);
+            stack.Peak();
+            stack.Pop();
+            stack.Print();
+        }
+    }
+}
