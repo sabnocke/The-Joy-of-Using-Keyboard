9e37f436ac481493b557cef9f42aa6cc8375000d
sabnocke <hloch.michal@gmail.com>
2023-01-08 21:00:03 +0100

    actual update

--- C# more like E/Node/Program.cs
+++ C# more like E/Node/Program.cs
@@ -6,17 +6,17 @@ namespace Node
     {
         static void Main(string[] args)
         {
-            Node node = new Node(10);          
-            node.Peak();
-            node.Push(5);
-            node.Peak();
-            node.Push(12);
-            node.Push(8);
-            node.Peak();
-            node.Pop();
-            node.Peak();
-            node.integCck();
+            Node node = new Node(6);
+            node.Push(9);
+            node.Push(3);
+            node.Push(4);
+            node.Push(2);
+            node.Push(10);
+            node.Push(1);
             node.Print();
+            node.bubble_sort();
+            node.Print();
+            node.integCck();
         }
     }
 }
