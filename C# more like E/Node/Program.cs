228485027667b5bdd3193529c7088a1161956f0a
sabnocke <hloch.michal@gmail.com>
2023-01-09 17:59:48 +0100

    update V3

--- C# more like E/Node/Program.cs
+++ C# more like E/Node/Program.cs
@@ -1,22 +1,58 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace Node
 {
     internal class Program
     {
+        /*
+         * Co to vlastne umi?
+         * 
+         * .Push = push
+         * .Peak = peak
+         * .Print = print
+         * .Pop = pop
+         * .integCck = overi jestli na sebe vsechno navazuje
+         * Sorty: .callQuicksort = quicksort ; .bubble_sort = bubblesort
+         * (lepsi zpusob nemam)
+         * 
+         * .GenerateLst = generuje list nahodnych cisel a pak je vlozi do stacku
+         * .GenerateArr = --||-- ale s array (technicky rychlejsi)
+         * (parametry to upravuji)
+         */
         static void Main(string[] args)
+        {                               // pro lepsi orientaci
+            Node node = new();          // bubble sort, List
+            Node secondNode = new();    // quicksort, List
+            Node thirdNode = new();     // bubblesort, array
+            Node fourthNode = new();    // quicksort, list
+
+            Func<bool, bool> method = node.bubble_sort;
+            Func<bool, bool> methodQuick = secondNode.callQuicksort;
+            Func<bool, bool> methodArrBubble = thirdNode.bubble_sort;
+            Func<bool, bool> methodArrQuick = fourthNode.callQuicksort;
+            //Never seen a delegate???
+
+            node.GenerateLst(max_value: 500000,min_value:1);
+            Console.WriteLine($"Runtime: {measure(method).TotalMilliseconds} ms");
+            
+            secondNode.GenerateLst(max_value:500000);
+            Console.WriteLine($"Runtime: {measure(methodQuick).TotalMilliseconds} ms");
+            
+            thirdNode.GenerateArr(max_value:500000);
+            Console.WriteLine($"Runtime: {measure(methodArrBubble,true).TotalMilliseconds} ms");
+            
+            thirdNode.GenerateArr(max_value:500000);
+            Console.WriteLine($"Runtime: {measure(methodArrQuick,true).TotalMilliseconds} ms");
+        }
+        
+        public static TimeSpan measure(Func<bool, bool> method, bool sw = false)
         {
-            Node node = new Node(6);
-            node.Push(9);
-            node.Push(3);
-            node.Push(4);
-            node.Push(2);
-            node.Push(10);
-            node.Push(1);
-            node.Print();
-            node.bubble_sort();
-            node.Print();
-            node.integCck();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            method(sw);
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
         }
     }
 }
