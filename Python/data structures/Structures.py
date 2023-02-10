b5562140e6f75f513848c95990b5648a809d6a61
sabnocke <hloch.michal@gmail.com>
2023-02-10 20:20:04 +0100

    update

---
+++ Python/data structures/Structures.py
@@ -0,0 +1,117 @@
+class Node:
+    """Generic Node class
+
+    :param data: object to input
+    :type data: object
+
+    :param _next: following node
+    :type _next: Node or None
+
+    :param _prev: previous node
+    :type _prev: Node or None
+
+    """
+    def __init__(self, data=None, _next=None, _prev=None):
+        self.data: object = data
+        self.next: Node | None = _next
+        self.prev: Node | None = _prev
+
+    def __call__(self, *args, **kwargs):
+        return self(None, None, None)
+
+
+class StackError(ValueError):
+    pass
+
+
+class StackUnderflow(StackError):
+    pass
+
+
+class StackOverflow(StackError):
+    pass
+
+
+class Stack:
+    """Stack implementation
+
+    :raise StackUnderflow: if stack is empty
+    """
+    def __init__(self):
+        self.current = Node()
+        self.count: int = 0
+
+    def __repr__(self) -> str:
+        return f"top: {self.current.data}"
+
+    def __len__(self):
+        return self.count
+
+    def push(self, data):
+        new_node = Node(data=data, _next=None, _prev=None)
+        if self.current is not None:
+            new_node.prev = self.current
+            self.current.next = new_node
+        self.current = new_node
+        self.count += 1
+
+    # noinspection PyUnresolvedReferences
+    def pop(self):
+        """"""
+        temp = self.current
+        if self.count == 1:
+            self.current = None
+        if self.count > 1:
+            self.current = self.current.prev
+        if self.count < 1:
+            raise StackUnderflow("Attempted to call pop on empty stack")
+        return temp
+
+
+class Queue:
+    """
+    A class to create a queue.
+    """
+    def __init__(self):
+        self.tail = None
+        self.head = None
+        self.count = 0
+
+    def __repr__(self):
+        return f"{self.head.data}"
+
+    def __len__(self):
+        """value to return on len() call"""
+        return self.count
+
+    def enqueue(self, data: object):
+        """
+        Adds new element into the queue
+
+        :arg data: object to insert into queue
+        :type data: object
+        """
+        new_node = Node(data=data, _next=None, _prev=None)
+        if self.head is None:
+            self.head = new_node
+            self.tail = self.head
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+        self.count += 1
+
+    def dequeue(self):
+        """Removes appropriate element from queue
+
+        :raise StackUnderflow: if queue is empty
+        """
+        if self.count == 1:
+            self.head = None
+            self.tail = None
+        elif self.count > 1:
+            self.head = self.head.next
+            self.head.prev = None
+        elif self.count < 1:
+            raise StackUnderflow("Attempted to call dequeue on empty queue")
+        self.count -= 1
--- Python/sort.py
+++ Python/sort.py
@@ -1,235 +1,225 @@
-# swap
-def swap(array: list[int], first: int, second: int) -> None:
+class SortAlgo:
+    """
+    collection of sorting algorithms
+
+    :arg lst: list to be sorted
     """
 
-    :rtype: object
-    :param array: array of ints
-    :param first: first index
-    :param second: second index
-    """
-    array[first], array[second] = array[second], array[first]
+    def __init__(self, lst):
+        self.lst = lst
 
+    # swap
+    def swap(self, first: int, second: int) -> None:
+        """
+        swaps values in list
 
-def test_ascent(arr) -> bool:
-    """
-    
-    :param arr: list to test
-    :return: True if passes, else False; Actually prints tho
-    """
-    return all(arr[i] <= arr[i + 1] for i in range(len(arr) - 1))
+        :param first: first index
+        :param second: second index
+        """
+        self.lst[first], self.lst[second] = self.lst[second], self.lst[first]
 
+    def __len__(self):
+        """returns length of input list"""
+        return len(self.lst)
 
-# bubble sort
-def bubble_sort(array: list) -> list:
-    for _ in range(len(array) - 1):
-        for i in range(len(array) - 1):
-            if array[i] > array[i + 1]:
-                swap(array, i, i + 1)
-    return array
+    def __repr__(self):
+        method_list = [method for method in dir(self) if method.startswith('__') is False and
+                       method.endswith('sort') is True]
+        return '\n'.join(method_list)
 
+    def ascent(self, to_test) -> bool:
+        """ tests if list is in ascending order
 
-# shaker sort
-def shaker_sort(array: list[int]) -> list:
-    start, end = 0, len(array) - 1
-    swapped: bool = True
-    while swapped:
-        swapped = False
-        for i in range(start, end):
-            if array[i] > array[i + 1]:
-                swap(array, i, i + 1)
-                swapped = True
+        :return: True if passes, else False;
+        """
+        return all(to_test[i] <= to_test[i + 1] for i in range(len(self) - 1))
 
-        if not swapped:
-            break
+    # bubble sort
+    def bubble_sort(self) -> list:
+        for _ in range(len(self) - 1):
+            for i in range(len(self) - 1):
+                if self.lst[i] > self.lst[i + 1]:
+                    self.swap(i, i + 1)
+        return self.lst
 
-        swapped = False
-        end -= 1
-
-        for a in range(end, start, -1):
-            if array[a] < array[a - 1]:
-                swap(array, a, a - 1)
-                swapped = True
-        if not swapped:
-            break
-        start += 1
-    return array
-
-
-# insert sort
-def insert_sort(arr: list[int]) -> list:
-    def binary_search(flg_lst: list[int], target: int, low: int, high: int):
-        while low <= high:
-            mid = low + (high - low) // 2
-            if target == flg_lst[mid]:
-                return mid + 1
-            elif target > flg_lst[mid]:
-                low = mid + 1
-            else:
-                high = mid - 1
-        return low
-
-    def insSort(flg_lst: list[int]):
-        lng = len(flg_lst)
-        for i in range(lng):
-            j = i - 1
-            selected = flg_lst[i]
-            loc = binary_search(flg_lst, selected, 0, j)
-
-            while j >= loc:
-                flg_lst[j + 1] = flg_lst[j]
-                j -= 1
-            flg_lst[j + 1] = selected
-        return flg_lst
-
-    return insSort(arr)
-
-
-# select sort
-def selection_sort(arr: list[int]) -> list:
-    def selSort(flg_lst: list[int]):
-        loc = 0
-        length: int = len(flg_lst)
-        while loc < length - 1:
-            j: int = locOfSmallest(flg_lst, loc, length - 1)
-            swap(flg_lst, loc, j)
-            loc += 1
-        return arr
-
-    def locOfSmallest(flg_lst: list[int], start: int, end: int) -> int:
-        i: int = start
-        j: int = i
-        while i <= end:
-            if flg_lst[i] < flg_lst[j]:
-                j = i
-            i += 1
-        return j
-
-    return selSort(arr)
-
-
-# merge sort
-def merge_sort(arr: list[int]) -> list:
-    def mrgSort(flg_lst):
-        if len(flg_lst) <= 1:
-            return flg_lst
-        mid = len(flg_lst) // 2
-        left = mrgSort(flg_lst[:mid])
-        right = mrgSort(flg_lst[mid:])
-        return merge(left, right)
-
-    def merge(left, right):
-        result = []
-        i = j = 0
-        while i < len(left) and j < len(right):
-            if left[i] <= right[j]:
-                result.append(left[i])
-                i += 1
-            else:
-                result.append(right[j])
-                j += 1
-        result += left[i:]
-        result += right[j:]
-        return result
-
-    return mrgSort(arr)
-
-
-# quick sort
-def quick_sort(data: list) -> list:
-    start = 0
-    end = len(data) - 1
-
-    def partition(arr, low, high):
-        mid = (low + high) // 2
-        i_left = low
-        i_right = high
-        pivot = arr[mid]
-        while i_left < i_right:
-            while arr[i_left] < pivot:
-                i_left += 1
-            while arr[i_right] > pivot:
-                i_right -= 1
-            if i_left <= i_right:
-                swap(arr, i_left, i_right)
-                i_left += 1
-                i_right -= 1
-        return i_left
-
-    def qSort(arr: list[int], low: int, high: int):
-        mid_point = partition(arr, low, high)
-        if low < mid_point - 1:
-            qSort(arr, low, mid_point - 1)
-        if mid_point < high:
-            qSort(arr, mid_point, high)
-        pivot = mid
-        while low < high:
-            while arr[low] < arr[pivot]:
-                low += 1
-            while arr[high] > arr[pivot]:
-                high -= 1
-            while low <= high:
-                swap(arr, low, high)
-                low += 1
-                high -= 1
-        return low
-
-    def qSort(arr: list[int], low: int, high: int):
-        mid_point = partition(arr, low, high)
-        if  low < high:
-            qSort(arr, low, mid_point - 1)
-            qSort(arr, mid_point, high)
-            print("high")
-        return arr
-
-    return qSort(data, start, end)
-
-
-# comb sort
-def comb_sort(arr: list[int]) -> list:
-    def getNextGap(gap):
-        gap = (gap * 10) // 13
-        return max(gap, 1)
-
-    def cSort(flg_lst):
-        n = len(flg_lst)
-        gap = n
-        swapped = True
-        while gap != 1 and swapped == 1:
-            gap = getNextGap(gap)
+    # shaker sort
+    def shaker_sort(self) -> list:
+        start, end = 0, len(self) - 1
+        swapped: bool = True
+        while swapped:
             swapped = False
-            for i in range(n - gap):
-                if flg_lst[i] > arr[i + gap]:
-                    swap(flg_lst, i, i + gap)
+            for i in range(start, end):
+                if self.lst[i] > self.lst[i + 1]:
+                    self.swap(i, i + 1)
                     swapped = True
-        return flg_lst
 
-    return cSort(arr)
+            if not swapped:
+                break
+
+            swapped = False
+            end -= 1
+
+            for a in range(end, start, -1):
+                if self.lst[a] < self.lst[a - 1]:
+                    self.swap(a, a - 1)
+                    swapped = True
+            if not swapped:
+                break
+            start += 1
+        return self.lst
+
+    # insert sort
+    def insert_sort(self) -> list:
+        def binary_search(flg_lst: list[int], target: int, low: int, high: int):
+            while low <= high:
+                mid = low + (high - low) // 2
+                if target == flg_lst[mid]:
+                    return mid + 1
+                elif target > flg_lst[mid]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            return low
+
+        def insSort():
+            lng = len(self)
+            for i in range(lng):
+                j = i - 1
+                selected = self.lst[i]
+                loc = binary_search(self.lst, selected, 0, j)
+
+                while j >= loc:
+                    self.lst[j + 1] = self.lst[j]
+                    j -= 1
+                self.lst[j + 1] = selected
+            return self.lst
+
+        return insSort()
+
+    # select sort
+    def selection_sort(self) -> list:
+        def selSort():
+            loc = 0
+            length: int = len(self)
+            while loc < length - 1:
+                j: int = locOfSmallest(self.lst, loc, length - 1)
+                self.swap(loc, j)
+                loc += 1
+            return self.lst
+
+        def locOfSmallest(flg_lst: list[int], start: int, end: int) -> int:
+            i: int = start
+            j: int = i
+            while i <= end:
+                if flg_lst[i] < flg_lst[j]:
+                    j = i
+                i += 1
+            return j
+
+        return selSort()
+
+    # merge sort
+    def merge_sort(self) -> list:
+
+        # noinspection PyUnusedLocal
+        def mrgSort(*args: int):
+            if len(self) <= 1:
+                return self.lst
+            mid = len(self) // 2
+            left = mrgSort(self.lst[:mid])
+            right = mrgSort(self.lst[mid:])
+            return merge(left, right)
+
+        def merge(left, right):
+            result = []
+            i = j = 0
+            while i < len(left) and j < len(right):
+                if left[i] <= right[j]:
+                    result.append(left[i])
+                    i += 1
+                else:
+                    result.append(right[j])
+                    j += 1
+            result += left[i:]
+            result += right[j:]
+            return result
+
+        return mrgSort()
+
+    # quick sort
+    def quick_sort(self) -> list:
+        start = 0
+        end = len(self) - 1
+
+        def partition(arr, low, high):
+            mid = (low + high) // 2
+            i_left, i_right = low, high
+            pivot = arr[mid]
+            while i_left < i_right:
+                while arr[i_left] < pivot:
+                    i_left += 1
+                while arr[i_right] > pivot:
+                    i_right -= 1
+                if i_left <= i_right:
+                    self.swap(i_left, i_right)
+                    i_left += 1
+                    i_right -= 1
+            return i_left
+
+        def qSorting(arr: list[int], low: int, high: int):
+            mid_point = partition(arr, low, high)
+            if low < high:
+                qSorting(arr, low, mid_point - 1)
+                qSorting(arr, mid_point, high)
+            return arr
+
+        return qSorting(self.lst, start, end)
+
+    # comb sort
+    def comb_sort(self) -> list:
+        def getNextGap(gap):
+            gap = (gap * 10) // 13
+            return max(gap, 1)
+
+        def cSort():
+            n: int = len(self)
+            gap = n
+            swapped = True
+            while gap != 1 and swapped == 1:
+                gap = getNextGap(gap)
+                swapped = False
+                for i in range(n - gap):
+                    if self.lst[i] > self.lst[i + gap]:
+                        self.swap(i, i + gap)
+                        swapped = True
+            return self.lst
+
+        return cSort()
+
+    # pigeonhole sort
+    def pigeonhole_sort(self):
+        """
+        Similar to Counting sort, although it should work with negative numbers
+        :raise TypeError: input should be int
+        :return: sorted arr
+        """
+        min_ = min(self.lst)
+        max_ = max(self.lst)
+        range_ = max_ - min_ + 1
+        holes = [0] * range_
+        for x in self.lst:
+            if x is not int:
+                raise TypeError("Should always be int")
+            holes[x - min_] += 1
+        i = 0
+        for count in range(range_):
+            while holes[count] > 0:
+                holes[count] -= 1
+                self.lst[i] = count + min_
+                i += 1
+        return self.lst
 
 
-# pigeonhole sort
-def pigeonhole_sort(arr: list):
-    min_ = min(arr)
-    max_ = max(arr)
-    range_ = max_ - min_ + 1
-    holes = [0] * range_
-    for x in arr:
-        assert type(x) is int, "int only!"
-        holes[x - min_] += 1
-    i = 0
-    for count in range(range_):
-        while holes[count] > 0:
-            holes[count] -= 1
-            arr[i] = count + min_
-            i += 1
-    return arr
-
-
-lst = [8, 4, 1, 56, 3, -44, 23, -6, 28, 0]
-lst2 = [10, 2, 5, 89, 1496, 256, 7, 89]
-quick_sort(lst)
-print(lst)
-bubble_sort(lst)
-print(lst)
-
-
-
+test: list[int] = [8, 4, 1, 56, 3, -44, 23, -6, 28, 0]
+sort = SortAlgo(test)
+print(sort)
