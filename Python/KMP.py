21d89a1fd941d05a544a8a046b98567d34d0ed53
sabnocke <hloch.michal@gmail.com>
2023-03-18 14:30:36 +0100

    Update

--- Python/KMP.py
+++ Python/KMP.py
@@ -8,12 +8,13 @@ def kmp_search(text, pattern):
             index_p += 1
         if index_p == pattern_length:
             print("Podřetězec nalezen na pozici", index_t - index_p)
-            index_p = next[index_p-1]
+            index_p = next[index_p - 1]
         elif index_t < text_length and text[index_t] != pattern[index_p]:
             if index_p != 0:
-                index_p = next[index_p-1]
+                index_p = next[index_p - 1]
             else:
                 index_t += 1
+
 
 def compute_prefix_function(pattern):
     # vraci list prefixu, napr. "kokokosovy":"kokos" = [0, 0, 1, 2, 0]
@@ -29,9 +30,10 @@ def compute_prefix_function(pattern):
             next[rnd_i] = 0
             rnd_i += 1
         else:
-            rnd_j = next[rnd_j-1]
+            rnd_j = next[rnd_j - 1]
     print(next)
     return next
+
 
 text = "kokokosovy"
 kmp_search(text, "kokos")
